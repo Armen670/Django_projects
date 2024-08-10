606e66e0c86d0d25452d104eeab28e5ad1a33072
Armen670 <armen.baburian@yandex.ru>
2024-08-10 12:38:43 +0300

    new projects

--- second_music_app/DjangoMusicPlayer/musicapp/admin.py
+++ second_music_app/DjangoMusicPlayer/musicapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Song
 # Register your models here.
+@admin.register(Song)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = 'pk','audio_link','title','artist'
+    list_display_links = 'pk','audio_link','title','artist'
